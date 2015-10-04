f747ccdcfd4f13b5ba4818757a13265c18da3876
mpaulweeks <mpaulweeks@gmail.com>
2015-10-04 17:30:16 -0400

    lookin better!

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@
 var store = Module('store');
 var repo = Module('repo');
 
+function str_format(str) {
+    var args = arguments;
+    return str.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+}
+
 function get_img_tag(card){
-    return '<img class="cardimage" '
-        + 'alt="' + card.id + '" '
-        + 'src="'
-        + repo.get_img_url(card)
-        + '"><img/>';
+    var template = '<a href="http://magiccards.info/query?q={1}" target="_blank"><img class="cardimage" alt="{1}" src="{2}"><img/></a>';
+    return str_format(template, card.name, repo.get_img_url(card));
 }
 
 function run(data_file){
@@ -15,7 +19,7 @@ function run(data_file){
         var now = new Date();
         var cards = repo.get_by_date_and_status(now, repo.IN_STACK);
         for (var i = 0; i < cards.length; i++){
-            $('body').append(get_img_tag(cards[i]));
+            $('.cardlistdisplay').append(get_img_tag(cards[i]));
         }
     });
 }
