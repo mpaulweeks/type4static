6da25c1f5fc7e5b2ccaffa65473711cd35730cf0
mpaulweeks <mpaulweeks@gmail.com>
2015-10-17 14:46:45 -0400

    make script dir

--- script/multiverse_gen.py
+++ script/multiverse_gen.py
@@ -1,7 +1,7 @@
 import json
 
 out = {}
-with open("AllSets.json") as json_file:
+with open("json/AllSets.json") as json_file:
     sets = json.load(json_file)
 for card_set in sets.values():
     for card in card_set["cards"]:
@@ -10,5 +10,5 @@ for card_set in sets.values():
         if mid:
             out[name] = mid
 
-with open("multiverse_ids.json", "w") as json_file:
+with open("json/multiverse_ids.json", "w") as json_file:
     json.dump(out, json_file)
